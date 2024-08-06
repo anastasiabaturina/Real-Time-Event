20e819dd768a4f2594711d5ab749a77977c23e86
Anastasia Baturina <a.baturina@sh.dunice.net>
2024-08-06 10:18:32 +0300

    added client and hub

---
+++ RealTimeEvent/Hub/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+ 
+namespace SignalRApp
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message)
+        {
+            await Clients.All.SendAsync("Receive", message, Context.ConnectionId);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("Notify", $"{Context.ConnectionId} вошел в чат");
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await Clients.All.SendAsync("Notify", $"{Context.ConnectionId} покинул в чат");
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
